888629c5a1f405f0aec1e1531c5643ff7cb2bce2
Adrian Torres <des.4drian@gmail.com>
2025-06-27 18:05:51 -0500

    Update UserServiceTest.java

--- src/test/java/com/desfid/api_rest_bdd/services/UserServiceTest.java
+++ src/test/java/com/desfid/api_rest_bdd/services/UserServiceTest.java
@@ -1,11 +1,8 @@
 package com.desfid.api_rest_bdd.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
